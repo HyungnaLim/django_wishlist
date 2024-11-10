b07302a5ae4673ab775b8e11d107eecdf550c645
Hyungna <hyungnalim@gmail.com>
2024-11-09 21:38:00 -0600

    visited places and 404 response

--- wishlist/travel_wishlist/urls.py
+++ wishlist/travel_wishlist/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.place_list, name='place_list'),   # home page
     path('about', views.about, name='about'),
-    path('visited', views.places_visited, name='places_visited')
+    path('visited', views.places_visited, name='places_visited'),
+    path('place/<int:place_pk>/was_visited', views.place_was_visited, name='place_was_visited')
 ]
--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Place
 from .forms import NewPlaceForm
 
@@ -24,3 +24,11 @@ def about(request):
 def places_visited(request):
     visited = Place.objects.filter(visited=True).order_by('name')
     return render(request, 'travel_wishlist/visited.html', {'visited':visited})
+
+def place_was_visited(request, place_pk):
+    if request.method == 'POST':
+        # place = Place.objects.get(pk=place_pk)
+        place = get_object_or_404(Place, pk=place_pk)   # if object is not found, return 404 error response
+        place.visited = True
+        place.save()
+    return redirect('place_list')   # redirect to the path 'place_list'
